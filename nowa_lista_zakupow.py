deace10440ef89f79b71fa4438a37082f12cad1a
Rafał Raźny <rafal-razny@wp.pl>
2021-12-21 22:39:53 +0100

    Add new shop and values nowa_lista_zakupow.py

--- nowa_lista_zakupow.py
+++ nowa_lista_zakupow.py
@@ -1,16 +1,20 @@
 lista_zakupow = {
     "intersport": ["skarpety", "bluza", "gogle", "kijki"],
-    "apteka": ["aspiryna", "multiwitamina", "rutinoscorbin"]
+    "apteka": ["aspiryna", "multiwitamina", "rutinoscorbin"],
+    "carrefour": ["wódka", "pepsi", "lód"]
 }
 print("Lista zakupów")
 for keys, values in lista_zakupow.items():
   if keys == "intersport":
     print(f"Idę do {keys.title()}, kupuję tu następujące rzeczy: {str(values).title()}.")
-    
 
 for keys, values in lista_zakupow.items():
   if keys == "apteka":
     print (f"Idę do {keys.title()}, kupuję tu następujące rzeczy: {str(values).title()}.")
+
+for keys, values in lista_zakupow.items():
+    if keys == "carrefour":
+        print(f"Idę do {keys.title()}, kupuję tu następujące rzeczy {str(values).title()}.")
 
 number = []
 for x in lista_zakupow.values():
